b637e893e617a4cfdd0d23cb51c4f5d3639c668a
MCorange99 <gvidasjuknevicius2@gmail.com>
2023-12-30 01:46:49 +0200

    Refractored the code a bit to be able to make several types of uuid's without repeating code

--- src/app/tools/uuid-generator/UuidGeneratorComponent.tsx
+++ src/app/tools/uuid-generator/UuidGeneratorComponent.tsx
@@ -1,28 +1,45 @@
 "use client";
 
 import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import * as uuid  from "uuid";
 import ReadOnlyTextArea from "@/app/components/common/ReadOnlyTextArea";
 
-export default function UuidGeneratorComponent() {
-const [uuid, setUuid] = useState(uuidv4());
-const generateNewAndCopy = () => {
-  const newUuid = uuidv4();
-  setUuid(newUuid);
-  navigator.clipboard.writeText(newUuid);
-}
+function UuidGenerator({name, fn}: {name: string, fn: () => string}) { // ts is dumb, why cant it take the type directly
+  const [uuid, setUuid] = useState(fn());
+
+  const generateNewAndCopy = (fn: () => string) => {
+    const newUuid = fn();
+    setUuid(newUuid);
+    navigator.clipboard.writeText(newUuid);
+  }
+
   return (
-      <div className="w-full flex flex-col gap-4">
-        <ReadOnlyTextArea
-            value={uuid}
-            title="UUIDv4"
-        />
-        <button
-          type="button"
-          className="h-11 rounded-md mt-4 bg-indigo-500 px-3.5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-          onClick={generateNewAndCopy}>
-        Generate & Copy
-        </button>
-      </div>
+    <div className="w-full flex flex-col gap-4">
+      <ReadOnlyTextArea
+          value={uuid}
+          title={name}
+      />
+      <button
+        type="button"
+        className="h-11 rounded-md mt-4 bg-indigo-500 px-3.5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+        onClick={() => generateNewAndCopy(fn)}>
+      Generate & Copy
+      </button>
+    </div>
+  );
+}
+
+
+
+export default function UuidGeneratorComponent() {
+  
+
+  return (
+    <>
+      <UuidGenerator name="UUIDv1" fn={uuid.v1}/>
+      // TODO: add v3
+      <UuidGenerator name="UUIDv4" fn={uuid.v4}/>
+      // TODO: add v5
+    </>
   );
 }
